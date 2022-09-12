74160a7f216eae849bcd60e444b01cb3e2b56f99
Tomas Assenza <tomyassenza@gmail.com>
2022-09-11 21:50:00 -0300

    Agregada pantalla de simular plazo fijo

---
+++ app/src/main/java/com/practicadappm/bancoutn/Simular_Plazo_Fijo.java
@@ -0,0 +1,13 @@
+package com.practicadappm.bancoutn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Simular_Plazo_Fijo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simular_plazo_fijo);
+    }
+}
