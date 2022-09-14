f041fd6b6be7586436cb38789592d3cc66ddfb06
Valentin <valefontana15@gmail.com>
2022-09-14 13:10:08 -0300

    Agregado binding y bundle en SimularPlazoFijo

--- app/src/main/java/com/practicadappm/bancoutn/SimularPlazoFijo.java
+++ app/src/main/java/com/practicadappm/bancoutn/SimularPlazoFijo.java
@@ -2,12 +2,38 @@ package com.practicadappm.bancoutn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Simular_Plazo_Fijo extends AppCompatActivity {
+import com.practicadappm.bancoutn.databinding.ActivityMainBinding;
+import com.practicadappm.bancoutn.databinding.ActivitySimularPlazoFijoBinding;
 
+public class SimularPlazoFijo extends AppCompatActivity {
+    private EditText capital, tasaNominal, tasaEfectiva;
+    private TextView simulaPlazo, simulaCapital, simulaIntereses, simulaTotal, simulaTotalAnual, monedaTextView;
+    private String moneda;
+    private ActivitySimularPlazoFijoBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simular_plazo_fijo);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            moneda = extras.getString("moneda");
+        }
+        binding = ActivitySimularPlazoFijoBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        capital = binding.capital;
+        tasaNominal = binding.tasaNominal;
+        tasaEfectiva = binding.tasaEfectiva;
+        simulaCapital = binding.simulaCapital;
+        simulaIntereses = binding.simulaIntereses;
+        simulaPlazo = binding.simulaPlazo;
+        simulaTotal = binding.simulaTotal;
+        simulaTotalAnual = binding.simulaTotalAnual;
+        monedaTextView = binding.monedaTextView;
+        monedaTextView.setText(moneda);
+
     }
 }
