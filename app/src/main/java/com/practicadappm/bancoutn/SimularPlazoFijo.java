c44aa16366ecb61a14a052fd0def988edff095c5
Valentin <valefontana15@gmail.com>
2022-09-28 11:49:07 -0300

    Agregada simulacion de plazo fijo

--- app/src/main/java/com/practicadappm/bancoutn/SimularPlazoFijo.java
+++ app/src/main/java/com/practicadappm/bancoutn/SimularPlazoFijo.java
@@ -2,8 +2,12 @@ package com.practicadappm.bancoutn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.practicadappm.bancoutn.databinding.ActivityMainBinding;
@@ -11,19 +15,25 @@ import com.practicadappm.bancoutn.databinding.ActivitySimularPlazoFijoBinding;
 
 public class SimularPlazoFijo extends AppCompatActivity {
     private EditText capital, tasaNominal, tasaEfectiva;
-    private TextView simulaPlazo, simulaCapital, simulaIntereses, simulaTotal, simulaTotalAnual, monedaTextView;
+    private Float capitalFloat= Float.valueOf(0), tasaNominalFloat = Float.valueOf(0), tasaEfectivaFloat = Float.valueOf(0);
+    private Integer cantMeses;
+    private TextView simulaPlazo, simulaCapital, simulaIntereses, simulaTotal, simulaTotalAnual, monedaTextView, cantDias;
+    private SeekBar meses;
     private String moneda;
     private ActivitySimularPlazoFijoBinding binding;
+    private Button confirmar;
+    private PlazoFijo plazoFijo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle extras = getIntent().getExtras();
-        if(extras != null){
+        if (extras != null) {
             moneda = extras.getString("moneda");
         }
         binding = ActivitySimularPlazoFijoBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
         capital = binding.capital;
         tasaNominal = binding.tasaNominal;
         tasaEfectiva = binding.tasaEfectiva;
@@ -33,7 +43,95 @@ public class SimularPlazoFijo extends AppCompatActivity {
         simulaTotal = binding.simulaTotal;
         simulaTotalAnual = binding.simulaTotalAnual;
         monedaTextView = binding.monedaTextView;
-        monedaTextView.setText(moneda);
+        cantDias = binding.cantidadDias;
+        meses = binding.seekDias;
+        confirmar = binding.botonConfirmar;
 
+
+        monedaTextView.setText(moneda);
+        capital.addTextChangedListener(textWatcher);
+        tasaNominal.addTextChangedListener(textWatcher);
+        tasaEfectiva.addTextChangedListener(textWatcher);
+        meses.setOnSeekBarChangeListener(listenerCambio);
+        plazoFijo = new PlazoFijo();
     }
+
+
+
+    //Cambio de TextViews
+    private void calcular() {
+
+        if(tasaNominal.getText().toString().trim().length() > 0)
+            tasaNominalFloat = Float.parseFloat(tasaNominal.getText().toString());
+        else
+            tasaNominalFloat = Float.valueOf(0);
+        if(tasaEfectiva.getText().toString().trim().length() > 0)
+            tasaEfectivaFloat = Float.parseFloat(tasaEfectiva.getText().toString());
+        else
+            tasaEfectivaFloat = Float.valueOf(0);
+        if(capital.getText().toString().trim().length() > 0)
+            capitalFloat = Float.parseFloat(capital.getText().toString());
+        else
+            capitalFloat = Float.valueOf(0);
+        cantMeses = meses.getProgress();
+
+        if(plazoFijo.setearYChequear(tasaNominalFloat, tasaEfectivaFloat, capitalFloat, cantMeses)) {
+            plazoFijo.simular();
+            simulaCapital.setText(plazoFijo.getCapital().toString());
+            simulaIntereses.setText(plazoFijo.getInteresesGanados().toString());
+            simulaPlazo.setText(plazoFijo.getDias().toString());
+            simulaTotal.setText(plazoFijo.getMontoTotal().toString());
+            simulaTotalAnual.setText(plazoFijo.getMontoTotalAnual().toString());
+            confirmar.setEnabled(true);
+        } else {
+            simulaCapital.setText("");
+            simulaIntereses.setText("");
+            simulaPlazo.setText("");
+            simulaTotal.setText("");
+            simulaTotalAnual.setText("");
+            confirmar.setEnabled(false);
+        }
+    }
+
+
+
+
+    //Listeners
+    private TextWatcher textWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        public void afterTextChanged(Editable s) {
+            calcular();
+        }
+    };
+
+    private SeekBar.OnSeekBarChangeListener listenerCambio = new SeekBar.OnSeekBarChangeListener() {
+
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            cantDias.setText(meses.getProgress()*30 + " dias");
+            calcular();
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+
+    };
 }
